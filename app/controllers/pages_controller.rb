b4d82a43b29386e7bbf2d74a97b507dc81f905f9
sinsoku <sinsoku.listy@gmail.com>
2017-02-20 15:17:55 +0900

    Fix N+1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
     @products = Product.order(created_at: :desc)
       .limit(10)
       .includes(:supplier)
+      .merge(includes: { supplier: :user_profiles }, where: ["supplier_type == ?", "User"])
   end
 end
